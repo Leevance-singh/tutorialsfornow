81e68b4bd51a5ae18d73b2402dbb2fef6844ff8f
Leevance-Singh <lsingh.ls629@gmail.com>
2024-10-06 23:41:31 +0530

    woah

--- crashing.js
+++ crashing.js
@@ -1,10 +1,2 @@
 console.log("Hey this is me !") ;
 console.log("This is what you came for !!");
-
-console.log("No this is what i came for !");
-///die with a smile !
-
-//************we still got much to learn ! *****/
-//woah this is truly wonderful 
-
-//why are we still here , just to suffer ! ?
--- crashingdust.c
+++ crashingdust.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 
 int main() {
-    
+    //this is not just a tutorial
     return 0;
 }
