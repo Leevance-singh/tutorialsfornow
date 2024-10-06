7cf90e73050e92b25f6cce695a55efd9ff2b16e6
Leevance-Singh <lsingh.ls629@gmail.com>
2024-10-06 22:19:34 +0530

    there is no any message it's just a tutoria;

--- crashing.js
+++ crashing.js
@@ -5,3 +5,6 @@ console.log("No this is what i came for !");
 ///die with a smile !
 
 //************we still got much to learn ! *****/
+//woah this is truly wonderful 
+
+why are we still here , just to suffer ! ?
